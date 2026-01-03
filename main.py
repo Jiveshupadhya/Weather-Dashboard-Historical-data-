ace4ee04d18b976e874bfb5da092b5522e7c5398
Jivesh Upadhyay <127330688+Jiveshupadhya@users.noreply.github.com>
2026-01-03 15:29:23 +0530

    basic page config using streamlit

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ import requests as rq
 import matplotlib.pyplot as plt
 import datetime as dt
 
+st.set_page_config(page_title ="Weather History Dashboard",
+                   page_icon ="🌦️",
+                   layout="centered")
+st.title=("🌍 Global Weather History Dashboard")
+st.write=("Analyse **monthly historical temperature and precipitation data**, valid data till 2024 only")
+
 
